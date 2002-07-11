932e0a1f719ae7daa9c5a0f7e694bb41c4df89f4
mdwelsh <mdwelsh>
2002-07-11 23:14:05 +0000

    Updating NBIO release script

--- RELEASE-NBIO.sh
+++ RELEASE-NBIO.sh
@@ -2,11 +2,10 @@
 
 # Generate a snapshot release of the 'mdw/nbio' directory
 
-RELEASE_VERSION=1.5
 BASE_DIR=$HOME/src/releases
-RELEASE=nbio-$RELEASE_VERSION
+RELEASE=nbio-release-`date +%Y%m%d`
 RELEASE_DIR=$BASE_DIR/$RELEASE
-PUBLIC_DIR=$HOME/public_html/proj/java-nbio/
+PUBLIC_DIR=$HOME/public_html/proj/java-nbio
 
 echo "Creating release in $RELEASE_DIR"
 
@@ -14,28 +13,26 @@ rm -rf $RELEASE_DIR
 mkdir -p $RELEASE_DIR
 cd $RELEASE_DIR
 echo "Unpacking from CVS archive..."
-#CVSROOT=ninja.cs:/disks/ninja/.CVS-ninja cvs -Q co mdw/nbio mdw/lib mdw/Makefile mdw/Makefile.include
-CVSROOT=/home/cs/mdw/repository cvs -Q co mdw/nbio mdw/lib mdw/Makefile mdw/Makefile.include
+export CVS_RSH=ssh
+cvs -z3 -d:ext:mdwelsh@cvs.seda.sourceforge.net:/cvsroot/seda -Q co seda/docs seda/lib seda/README seda/src/nbio seda/src/Makefile seda/src/Makefile.include
 find . -name CVS | xargs rm -r
 
 echo "Performing test build..."
-cd mdw
+cd seda
 make clean
-rm $RELEASE_DIR/mdw/lib/*
-export CLASSPATH=.:$RELEASE_DIR
-export LD_LIBRARY_PATH=$RELEASE_DIR/mdw/lib
+export CLASSPATH=.:$RELEASE_DIR/seda/src
+export LD_LIBRARY_PATH=$RELEASE_DIR/seda/lib
 make || { echo "Build of release failed with errors, exiting"; exit 1; }
 make clean
-rm $RELEASE_DIR/mdw/lib/*
-rm -rf $RELEASE_DIR/mdw/nbio/test/bench
+rm $RELEASE_DIR/seda/lib/*
 
 echo "Publishing javadoc documentation..."
-cd $RELEASE_DIR/mdw/nbio/javadoc
-make || { echo "Build of docs failed with errors, exiting"; exit 1; }
+cd $RELEASE_DIR/seda/docs/javadoc
+make nbio || { echo "Build of docs failed with errors, exiting"; exit 1; }
 rm -rf $PUBLIC_DIR/javadoc
-cd $RELEASE_DIR/mdw/nbio
+cd $RELEASE_DIR/seda/docs
 tar cf - javadoc | (cd $PUBLIC_DIR; tar xf -)
-cd $RELEASE_DIR/mdw/nbio/javadoc
+cd $RELEASE_DIR/seda/docs/javadoc
 make clean
 
 echo "Creating $RELEASE.tar.gz..."
@@ -45,6 +42,9 @@ rm -rf $RELEASE
 
 echo "Copying $RELEASE.tar.gz to $PUBLIC_DIR..."
 cp $RELEASE.tar.gz $PUBLIC_DIR
+
+echo "Don't forget to FTP $RELEASE to upload.sourceforge.net/incoming"
+echo "Done."
 
 #echo "Don't forget to tag CVS: cvs tag $RELEASE"
 #echo "Done."
