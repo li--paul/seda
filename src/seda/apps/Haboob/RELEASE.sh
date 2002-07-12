bb12a9ac00a56559a24cf8d50a1438fcb96d0fb7
mdwelsh <mdwelsh>
2002-07-12 17:43:17 +0000

    Checkin

--- src/seda/apps/Haboob/RELEASE.sh
+++ src/seda/apps/Haboob/RELEASE.sh
@@ -3,7 +3,7 @@
 # Generate a snapshot release of the Haboob directory
 
 BASE_DIR=$HOME/src/releases
-RELEASE=haboob-snapshot-`date +%Y%m%d`
+RELEASE=haboob-release-`date +%Y%m%d`
 RELEASE_DIR=$BASE_DIR/$RELEASE
 PUBLIC_DIR=$HOME/public_html/proj/seda/
 
@@ -13,14 +13,14 @@ rm -rf $RELEASE_DIR
 mkdir -p $RELEASE_DIR
 cd $RELEASE_DIR
 echo "Unpacking from CVS archive..."
-CVSROOT=ninja.cs:/disks/ninja/.CVS-ninja cvs -Q co ninja2/personal/mdw/apps/Haboob
+export CVS_RSH=ssh
+cvs -z3 -d:ext:mdwelsh@cvs.seda.sourceforge.net:/cvsroot/seda -Q co seda/src/seda/apps/Makefile seda/src/seda/apps/Haboob seda/src/seda/Makefile seda/src/seda/Makefile.include
 find . -name CVS | xargs rm -rf
-#rm -rf client/DATA-SOSP01
 
 echo "Performing test build..."
-cd ninja2/personal/mdw/apps/Haboob
+cd seda/src/seda/apps/Haboob
 make clean
-export CLASSPATH=.:$RELEASE_DIR:$HOME/src:$HOME/src/mdw/lib/collections.jar
+export CLASSPATH=.:$RELEASE_DIR/seda/src
 make || { echo "Build of release failed with errors, exiting"; exit 1; }
 make clean
 
@@ -33,4 +33,6 @@ echo "Copying $RELEASE.tar.gz to $PUBLIC_DIR..."
 cp $RELEASE.tar.gz $PUBLIC_DIR
 
 echo "Don't forget to tag CVS: cvs tag $RELEASE"
+echo "Don't forget to FTP $RELEASE to ftp://upload.sourceforge.net/incoming"
+
 echo "Done."
